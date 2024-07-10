edbec780d445ca6c6620e90756d73726dfdea5b6
Anna Hope <anna.me@hey.com>
2024-07-10 15:45:51 -0500

    Load URLs from main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env::args;
+
+use anyhow::{anyhow, Result};
+
+use browser::{load, URL};
+
+fn main() -> Result<()> {
+    let url = args()
+        .nth(1)
+        .ok_or(anyhow!("Please provide an argument for the URL"))
+        .map(|arg| URL::init(&arg))??;
+    load(&url)
 }
