bf8150ed7b8df63a4b4b1158d2c5aa78cbe2c4c9
Anna Hope <anna.me@hey.com>
2024-07-10 12:13:32 -0500

    Implement loading URLs

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,35 @@
-use anyhow;
+use std::collections::HashMap;
+use std::io;
+use std::io::{Read, Write};
+use std::net::TcpStream;
 
-pub(crate) struct URL {
+use anyhow::{Context, Result};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum URLError {
+    #[error("error splitting the URL: `{0}`")]
+    Split(String),
+
+    #[error("can't connect via TCP")]
+    ConnectionFailed(#[from] io::Error),
+
+    #[error("invalid TCP response: {0}")]
+    InvalidResponse(String),
+}
+
+#[derive(Debug)]
+pub struct URL {
     pub scheme: String,
     pub path: String,
     pub host: String,
 }
 
 impl URL {
-    pub fn init(url: &str) -> anyhow::Result<Self> {
+    pub fn init(url: &str) -> Result<Self> {
         let (scheme, url) = url
             .split_once("://")
-            .ok_or(anyhow::anyhow!("Couldn't split {url}"))?;
+            .ok_or_else(|| URLError::Split(url.to_string()))?;
         assert_eq!(scheme, "http");
 
         let url = if url.contains("/") {
@@ -21,7 +40,7 @@ impl URL {
 
         let (host, url) = url
             .split_once("/")
-            .ok_or(anyhow::anyhow!("Couldn't split {url}"))?;
+            .ok_or_else(|| URLError::Split(url.to_string()))?;
         let path = format!("/{url}");
 
         Ok(Self {
@@ -30,6 +49,63 @@ impl URL {
             path,
         })
     }
+
+    pub fn request(&self) -> Result<String> {
+        let mut stream = TcpStream::connect(format!("{}:80", self.host))?;
+        let request = format!("GET {} HTTP/1.0\r\nHost: {}\r\n\r\n", self.path, self.host);
+
+        stream.write_all(request.as_bytes())?;
+        let mut read_buf = String::new();
+        stream.read_to_string(&mut read_buf)?;
+        let mut lines = read_buf.lines();
+        let status_line = lines
+            .next()
+            .ok_or_else(|| URLError::InvalidResponse(read_buf.clone()))
+            .context("Can't parse status_line")?;
+        let (_version, _status, _explanation) = {
+            let parts = status_line.splitn(3, " ").collect::<Vec<_>>();
+            if parts.len() < 3 {
+                return Err(URLError::InvalidResponse(read_buf.clone()))
+                    .context(format!("Can't parse status_line parts: {status_line}",));
+            }
+            (parts[0], parts[1], parts[2])
+        };
+
+        let response_headers: HashMap<_, _> =
+            HashMap::from_iter(lines.by_ref().map_while(|line| {
+                if line == r"\r\n" {
+                    None
+                } else {
+                    let (header, value) = line.split_once(":")?;
+                    Some((header.to_lowercase(), value.trim().to_string()))
+                }
+            }));
+
+        assert!(!response_headers.contains_key("transfer-encoding"));
+        assert!(!response_headers.contains_key("content-encoding"));
+
+        let content = String::from_iter(lines);
+        Ok(content)
+    }
+}
+
+pub fn show(body: &str) {
+    let mut in_tag = false;
+    for c in body.chars() {
+        if c == '<' {
+            in_tag = true;
+        } else if c == '>' {
+            in_tag = false;
+        } else if !in_tag {
+            print!("{c}");
+        }
+    }
+}
+
+pub fn load(url: &URL) -> Result<()> {
+    let body = url.request()?;
+    show(&body);
+    Ok(())
 }
 
 #[cfg(test)]
@@ -42,5 +118,11 @@ mod tests {
         assert_eq!(url.scheme, "http");
         assert_eq!(url.host, "example.org");
         assert_eq!(url.path, "/");
+    }
+
+    #[test]
+    fn load_url() {
+        let url = URL::init("http://example.org").unwrap();
+        load(&url).unwrap();
     }
 }
