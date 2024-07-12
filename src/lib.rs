ee64074a7ce4081d44f5161442a32898917e0bf4
Anna Hope <anna.me@hey.com>
2024-07-12 15:48:12 -0500

    Refactor using Request and Response

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt::{Display, Formatter};
 use std::io;
 use std::io::{Read, Write};
 use std::net::TcpStream;
@@ -57,6 +58,161 @@ impl Scheme {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+pub enum RequestMethod {
+    GET,
+}
+
+impl Display for RequestMethod {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::GET => write!(f, "GET"),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Request {
+    pub method: RequestMethod,
+    headers: HashMap<String, Vec<String>>,
+    pub body: Option<String>,
+    pub url: URL,
+}
+
+impl Request {
+    fn init(method: RequestMethod, url: URL) -> Self {
+        Self {
+            method,
+            headers: HashMap::from([("Host".to_string(), vec![url.host.clone()])]),
+            body: None,
+            url,
+        }
+    }
+
+    /// Adds given Header key/values to the Request.
+    /// The same Header key can be specified multiple times.
+    /// Note that this does not overwrite any existing headers!
+    /// If a given Header already exists in this Request,
+    /// the new value(s) will simply be appended to that Header.
+    fn add_headers(mut self, headers: &[(&str, &str)]) -> Self {
+        for (header, value) in headers {
+            if let Some(existing_values) = self.headers.get_mut(*header) {
+                existing_values.push(value.to_string());
+            } else {
+                self.headers
+                    .insert(header.to_string(), vec![value.to_string()]);
+            }
+        }
+        self
+    }
+
+    fn with_body(mut self, body: &str) -> Self {
+        self.body = Some(body.to_string());
+        self
+    }
+
+    fn make(&self) -> Result<Response> {
+        let read_buf = {
+            let mut stream = TcpStream::connect(format!("{}:{}", self.url.host, self.url.port))?;
+            let mut read_buf = String::new();
+            match self.url.scheme {
+                Scheme::HTTP => {
+                    stream.write_all(self.to_string().as_bytes())?;
+                    stream.read_to_string(&mut read_buf)?;
+                }
+                Scheme::HTTPS => {
+                    let mut client = rustls::ClientConnection::new(
+                        CONFIG.clone(),
+                        self.url.host.clone().try_into()?,
+                    )?;
+
+                    let mut tls = rustls::Stream::new(&mut client, &mut stream);
+                    tls.write_all(self.to_string().as_bytes())?;
+                    tls.read_to_string(&mut read_buf)?;
+                }
+            }
+            read_buf
+        };
+        Response::from_str(&read_buf)
+    }
+}
+
+impl Display for Request {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        let mut string = format!("{} {} HTTP/1.0\r\n", self.method, self.url.path);
+        for (header, values) in self.headers.iter() {
+            for value in values {
+                string.push_str(format!("{header}: {value}\r\n").as_str());
+            }
+        }
+        // TODO add body
+        write!(f, "{string}\r\n")
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct StatusLine {
+    version: String,
+    status_code: u16,
+    explanation: String,
+}
+
+impl StatusLine {
+    fn from_str(string: &str) -> Result<Self> {
+        let (version, status, explanation) = {
+            let parts = string.splitn(3, ' ').collect::<Vec<_>>();
+            if parts.len() < 3 {
+                return Err(URLError::InvalidResponse(string.to_string()))
+                    .context(format!("Can't parse status_line parts: {string}"));
+            }
+            (parts[0], parts[1], parts[2])
+        };
+        let status_code = status.parse::<u16>()?;
+        Ok(Self {
+            version: version.to_string(),
+            status_code,
+            explanation: explanation.to_string(),
+        })
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Response {
+    status_line: StatusLine,
+    pub headers: HashMap<String, String>,
+    pub body: Option<String>,
+}
+
+impl Response {
+    pub fn from_str(string: &str) -> Result<Self> {
+        let mut lines = string.lines();
+        let status_line = lines
+            .next()
+            .ok_or_else(|| URLError::InvalidResponse(string.to_string()))
+            .context("Missing status_line")?;
+        let status_line = StatusLine::from_str(status_line)?;
+
+        let headers: HashMap<_, _> = HashMap::from_iter(lines.by_ref().map_while(|line| {
+            if line == r"\r\n" {
+                None
+            } else {
+                let (header, value) = line.split_once(':')?;
+                Some((header.to_lowercase(), value.trim().to_string()))
+            }
+        }));
+
+        assert!(!headers.contains_key("transfer-encoding"));
+        assert!(!headers.contains_key("content-encoding"));
+
+        let content = String::from_iter(lines);
+        Ok(Self {
+            status_line,
+            headers,
+            body: Some(content),
+        })
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct URL {
     pub scheme: Scheme,
@@ -96,62 +252,6 @@ impl URL {
             port,
         })
     }
-
-    pub fn request(&self) -> Result<String> {
-        let read_buf = {
-            let mut read_buf = String::new();
-            let request = format!("GET {} HTTP/1.0\r\nHost: {}\r\n\r\n", self.path, self.host);
-            match self.scheme {
-                Scheme::HTTP => {
-                    let mut stream = TcpStream::connect(format!("{}:{}", self.host, self.port))?;
-                    stream.write_all(request.as_bytes())?;
-                    stream.read_to_string(&mut read_buf)?;
-                }
-                Scheme::HTTPS => {
-                    let mut client = rustls::ClientConnection::new(
-                        CONFIG.clone(),
-                        self.host.clone().try_into()?,
-                    )?;
-
-                    let mut socket = TcpStream::connect(format!("{}:{}", self.host, self.port))?;
-                    let mut tls = rustls::Stream::new(&mut client, &mut socket);
-                    tls.write_all(request.as_bytes())?;
-                    tls.read_to_string(&mut read_buf)?;
-                }
-            }
-            read_buf
-        };
-
-        let mut lines = read_buf.lines();
-        let status_line = lines
-            .next()
-            .ok_or_else(|| URLError::InvalidResponse(read_buf.clone()))
-            .context("Can't parse status_line")?;
-        let (_version, _status, _explanation) = {
-            let parts = status_line.splitn(3, ' ').collect::<Vec<_>>();
-            if parts.len() < 3 {
-                return Err(URLError::InvalidResponse(read_buf.clone()))
-                    .context(format!("Can't parse status_line parts: {status_line}",));
-            }
-            (parts[0], parts[1], parts[2])
-        };
-
-        let response_headers: HashMap<_, _> =
-            HashMap::from_iter(lines.by_ref().map_while(|line| {
-                if line == r"\r\n" {
-                    None
-                } else {
-                    let (header, value) = line.split_once(':')?;
-                    Some((header.to_lowercase(), value.trim().to_string()))
-                }
-            }));
-
-        assert!(!response_headers.contains_key("transfer-encoding"));
-        assert!(!response_headers.contains_key("content-encoding"));
-
-        let content = String::from_iter(lines);
-        Ok(content)
-    }
 }
 
 pub fn show(body: &str) {
@@ -168,8 +268,14 @@ pub fn show(body: &str) {
 }
 
 pub fn load(url: &URL) -> Result<()> {
-    let body = url.request()?;
-    show(&body);
+    let request = Request::init(RequestMethod::GET, url.clone());
+    let response = request.make()?;
+    show(
+        response
+            .body
+            .ok_or_else(|| anyhow::anyhow!("Empty response body"))?
+            .as_str(),
+    );
     Ok(())
 }
 
@@ -215,4 +321,9 @@ mod tests {
         let url = URL::init("https://example.org").unwrap();
         load(&url).unwrap();
     }
+
+    // #[test]
+    // fn send_connection_user_agent_headers() {
+    //     let url = URL::init("https://example.org").unwrap();
+    // }
 }
