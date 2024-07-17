5cd9a29d4334d020347a82a94ed176b1857ee324
Anna Hope <anna.me@hey.com>
2024-07-16 23:40:48 -0500

    Move logic for handling WebUrl into its own function

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod url;
 use std::fs;
 
 use crate::request::RequestMethod;
-use crate::url::{Url, UrlError};
+use crate::url::{Url, WebUrl};
 use anyhow::{anyhow, Context, Result};
 
 // TODO: Check what real browsers set this to.
@@ -19,13 +19,11 @@ fn parse_body(body: &str, render: bool) -> Result<String> {
     let mut skip_entity = false;
 
     let mut result = String::new();
-    // TODO: Replace with something more efficient than effectively duplicating
-    // the whole input string.
-    let chars = body.chars().collect::<Vec<_>>();
+    let bytes = body.as_bytes();
 
     let mut current_index = 0;
-    while current_index < chars.len() {
-        let c = chars[current_index];
+    while current_index < bytes.len() {
+        let c = char::from(bytes[current_index]);
 
         if c == '&' {
             if skip_entity {
@@ -40,7 +38,7 @@ fn parse_body(body: &str, render: bool) -> Result<String> {
                 current_entity.push(c);
                 current_index += 1;
 
-                while let Some(&next_char) = chars.get(current_index) {
+                while let Some(next_char) = bytes.get(current_index).map(|b| char::from(*b)) {
                     current_entity.push(next_char);
                     current_index += 1;
                     if next_char == ';' || current_entity.len() == MAX_ENTITY_LEN {
@@ -81,46 +79,49 @@ fn parse_body(body: &str, render: bool) -> Result<String> {
     Ok(result)
 }
 
+/// Returns the body of a WebUrl, handling potential redirects.
+fn load_web_url(url: &WebUrl) -> Result<String> {
+    let mut request = request::Request::init(RequestMethod::Get, &url.host, true);
+    let mut response = request.make(url, None)?;
+    let mut status_code = response.status_code();
+    let mut num_redirects = 0;
+    let mut body = response.body.clone();
+
+    while (300..400).contains(&status_code) && num_redirects < MAX_REDIRECTS {
+        let new_url = response
+            .headers
+            .get("location")
+            .ok_or_else(|| anyhow!("Missing location in {response:?}"))?;
+
+        let new_url = if new_url.starts_with('/') {
+            Url::Web(url.with_path(new_url))
+        } else {
+            new_url.parse::<Url>()?
+        };
+
+        let new_url = new_url
+            .as_web_url()
+            .ok_or_else(|| anyhow!("Not a WebUrl: {new_url:?}"))
+            .context(anyhow!("{response:?}"))?;
+
+        response = request.make(new_url, None)?;
+        status_code = response.status_code();
+        body.clone_from(&response.body);
+        num_redirects += 1;
+    }
+
+    let parsed_body = parse_body(
+        body.ok_or_else(|| anyhow!("Empty response body"))?.as_str(),
+        true,
+    )?;
+    Ok(parsed_body)
+}
+
 fn load(url: &str) -> Result<String> {
     let url = url.parse::<Url>()?;
 
     match url {
-        Url::Web(url) => {
-            let mut request = request::Request::init(RequestMethod::Get, &url.host, true);
-            let mut response = request.make(&url, None)?;
-            let mut status_code = response.status_code();
-            let mut num_redirects = 0;
-            let mut body = response.body.clone();
-
-            while (300..400).contains(&status_code) && num_redirects < MAX_REDIRECTS {
-                let new_url = response
-                    .headers
-                    .get("location")
-                    .ok_or_else(|| anyhow!("Missing location in {response:?}"))?;
-
-                let new_url = if new_url.starts_with('/') {
-                    Url::Web(url.with_path(new_url))
-                } else {
-                    new_url.parse::<Url>()?
-                };
-
-                let new_url = new_url
-                    .as_web_url()
-                    .ok_or_else(|| anyhow!("Not a WebUrl: {new_url:?}"))
-                    .context(anyhow!("{response:?}"))?;
-
-                response = request.make(new_url, None)?;
-                status_code = response.status_code();
-                body.clone_from(&response.body);
-                num_redirects += 1;
-            }
-
-            let parsed_body = parse_body(
-                body.ok_or_else(|| anyhow!("Empty response body"))?.as_str(),
-                true,
-            )?;
-            Ok(parsed_body)
-        }
+        Url::Web(url) => load_web_url(&url),
         Url::File(url) => {
             let contents = fs::read(&url.path).context(url.path)?;
             let contents = String::from_utf8_lossy(&contents);
