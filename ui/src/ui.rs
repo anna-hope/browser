54f95dbb0dc76b3e81e3976db9d9f81887d45867
Anna Hope <anna.me@hey.com>
2024-07-22 14:14:55 -0500

    Add height and width request to window

--- ui/src/ui.rs
+++ ui/src/ui.rs
@@ -7,6 +7,8 @@ const TITLE: &str = "Octo";
 pub fn build_ui(app: &Application) {
     let window = ApplicationWindow::builder()
         .application(app)
+        .width_request(800)
+        .height_request(600)
         .title(TITLE)
         .build();
     window.present();
