8d7d9d632eca291fcb7c5b7e10f0eb677f540fec
Anna Hope <anna.me@hey.com>
2024-07-22 15:45:19 -0500

    Commit segfaulting TextViewBuilder code

--- browser/src/main.rs
+++ browser/src/main.rs
@@ -1,27 +1,37 @@
 use std::env;
+use std::rc::Rc;
 
 use anyhow::Result;
 use octo_engine::Engine;
 
-use octo_ui::build_ui;
 use octo_ui::gtk::prelude::*;
 use octo_ui::gtk::{glib, Application};
+use octo_ui::{build_ui, ui};
 
 const APP_ID: &str = "me.annahope.Octo";
 
-fn show(url: &str, engine: &mut Engine) -> Result<()> {
+fn show(url: &str, engine: &mut Engine, app: &Application) -> Result<()> {
     let body = engine.load(url)?;
     if let Some(body) = body {
-        println!("{body}");
+        let text_view = ui::build_text_view(body.as_str()); // This will segfault.
+        if let Some(window) = app.active_window() {
+            window.set_child(Some(&text_view));
+        }
     } else {
         eprintln!("The request returned an empty response");
     }
     Ok(())
 }
 
-fn main() -> glib::ExitCode {
-    let app = Application::builder().application_id(APP_ID).build();
+fn main() -> Result<glib::ExitCode> {
+    let mut engine = Engine::default();
+    let url = env::args().nth(1).unwrap_or_else(|| {
+        let current_dir = env::current_dir().expect("Failed to get current working directory");
+        format!("file://{}/LICENSE", current_dir.to_string_lossy())
+    });
 
+    let app = Application::builder().application_id(APP_ID).build();
     app.connect_activate(build_ui);
-    app.run()
+    show(&url, &mut engine, &app)?;
+    Ok(app.run())
 }
--- ui/src/ui.rs
+++ ui/src/ui.rs
@@ -1,8 +1,17 @@
 use gtk::prelude::*;
 
-use gtk::{Application, ApplicationWindow};
+use gtk::{Application, ApplicationWindow, TextBuffer, TextView};
 
 const TITLE: &str = "Octo";
+
+pub fn build_text_view(text: &str) -> TextView {
+    let buffer = TextBuffer::builder().text(text).build();
+    TextView::builder()
+        .editable(false)
+        .buffer(&buffer)
+        .cursor_visible(false)
+        .build()
+}
 
 pub fn build_ui(app: &Application) {
     let window = ApplicationWindow::builder()
@@ -11,5 +20,9 @@ pub fn build_ui(app: &Application) {
         .height_request(600)
         .title(TITLE)
         .build();
+
+    let text_view = build_text_view("test");
+    window.set_child(Some(&text_view));
+
     window.present();
 }
