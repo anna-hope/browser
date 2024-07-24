05d36ef02febdf2668a6b97d71e7e394ff8bc08e
Anna Hope <anna.me@hey.com>
2024-07-24 17:01:16 -0500

    Remove commented out code

--- browser/src/main.rs
+++ browser/src/main.rs
@@ -10,19 +10,6 @@ use octo_browser::Browser;
 
 const APP_ID: &str = "me.annahope.Octo";
 
-// type SharedEngine = Arc<Mutex<Engine>>;
-
-// fn show(
-//     engine: SharedEngine,
-//     request_recv: Receiver<&str>,
-//     response_sender: Sender<Result<Option<String>>>,
-// ) {
-//     while let Ok(url) = request_recv.recv() {
-//         let body = engine.lock().expect("poisoned").load(url);
-//         response_sender.send(body).expect("Failed to send the body");
-//     }
-// }
-
 fn main() -> Result<glib::ExitCode> {
     let app = Application::builder()
         .application_id(APP_ID)
