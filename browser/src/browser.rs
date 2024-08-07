ff7fa7f36d2bf13d0e009f60c880c053e8dfc5ae
Anna Hope <anna.me@hey.com>
2024-08-07 11:42:07 -0500

    Reset scroll when we re-enter URL

--- browser/src/browser.rs
+++ browser/src/browser.rs
@@ -55,6 +55,7 @@ impl eframe::App for Browser {
 
             let response = ui.add(egui::TextEdit::singleline(&mut self.url));
             if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
+                self.scroll = 0.;
                 match self.engine.load(&self.url) {
                     Ok(Some(tokens)) => {
                         self.processed_tokens =
