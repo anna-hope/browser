4f1f060ef93138e398540e211a0859f866bf583f
Anna Hope <anna.me@hey.com>
2024-08-08 09:44:47 -0500

    Use natural scrolling

--- browser/src/browser.rs
+++ browser/src/browser.rs
@@ -51,7 +51,7 @@ impl eframe::App for Browser {
             }
 
             // Mouse wheel
-            ui.input(|i| self.scroll = f32::max(self.scroll + i.smooth_scroll_delta.y, 0.));
+            ui.input(|i| self.scroll = f32::max(self.scroll - i.smooth_scroll_delta.y, 0.));
 
             let response = ui.add(egui::TextEdit::singleline(&mut self.url));
             if response.lost_focus() && ui.input(|i| i.key_pressed(egui::Key::Enter)) {
